554f690196b176d6f03e29fa2343c1fb3c24133b
sjcy2704 <a1834160@adelaide.edu.au>
2026-03-26 12:57:35 +1030

    Add getting the input from user in the command line using scanner
    This change adds the initial function of getting the input from the
    user. First input will always be the regex and the next inputs will be
    the strings to test. If regex is empty, it throws an error and exits
    with code 1.

--- RegexEngine.java
+++ RegexEngine.java
@@ -1,26 +1,51 @@
 import java.util.Scanner;
 
 public class RegexEngine {
-    private boolean verbose;
+  private boolean verbose;
 
-    public RegexEngine(boolean verbose) {
-        this.verbose = verbose;
+  public RegexEngine(boolean verbose) {
+    this.verbose = verbose;
+  }
+
+  public static void main(String[] args) {
+    boolean verbose = false;
+
+    // Check for verbose flag
+    if (args.length > 0 && args[0].equals("-v")) {
+      verbose = true;
     }
 
-    public static void main(String[] args) {
-        boolean verbose = false;
+    RegexEngine engine = new RegexEngine(verbose);
+    engine.run();
+  }
 
-        // Check for verbose flag
-        if (args.length > 0 && args[0].equals("-v")) {
-            verbose = true;
-        }
+  public void run() {
+    Scanner scanner = new Scanner(System.in);
 
-        RegexEngine engine = new RegexEngine(verbose);
-        engine.run();
+    try {
+      // get first input as regex
+      String regex = scanner.nextLine().trim();
+
+      if (regex.isEmpty()) {
+        System.err.println("Error: Regex is empty");
+        System.exit(1);
+      }
+
+      // construct e-nfa 
+
+      System.out.println("Ready");
+
+      while (scanner.hasNextLine()) {
+        String input = scanner.nextLine();
+
+        // two modes to process
+        // verbose
+        // normal
+
+        System.out.println("Read " + input);
+      }
+    } finally {
+      scanner.close();
     }
-
-    public void run() {
-        Scanner scanner = new Scanner(System.in);
-        scanner.close();
-    }
+  }
 }
